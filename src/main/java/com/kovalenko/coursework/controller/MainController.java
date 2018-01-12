33b79165965864db9668cccccb235ab33bb2f34f
AlexKazanovskiy <a.kazanovskiy@newline.tech>
2018-01-12 19:03:17 +0200

    Update for REST architecture

--- src/main/java/com/kovalenko/coursework/controller/MainController.java
+++ src/main/java/com/kovalenko/coursework/controller/MainController.java
@@ -1,29 +1,28 @@
 package com.kovalenko.coursework.controller;
 
 import com.kovalenko.coursework.service.MainService;
-import lombok.RequiredArgsConstructor;
-import org.springframework.stereotype.Controller;
 
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
-@Controller
-@RequiredArgsConstructor
+import java.util.Map;
+
+@RestController
+
 public class MainController {
 
     private final MainService mainService;
 
-    @GetMapping("/")
-    public String initialize() {
-        return "index";
+
+    public MainController(MainService mainService) {
+        this.mainService = mainService;
     }
 
     @PostMapping("/")
-    public String getCorpus(Model model, @RequestBody MultipartFile text) {
-        model.addAttribute("stat", mainService.generateStat(text));
-        return "index";
+    public Map<String, Integer> getCorpus(@RequestBody MultipartFile text) {
+        return mainService.generateStat(text);
     }
 }
--- src/main/java/com/kovalenko/coursework/service/impl/MainServiceImpl.java
+++ src/main/java/com/kovalenko/coursework/service/impl/MainServiceImpl.java
@@ -1,11 +1,11 @@
 package com.kovalenko.coursework.service.impl;
 
 import com.kovalenko.coursework.service.MainService;
-import lombok.SneakyThrows;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.util.*;
@@ -19,33 +19,34 @@ public class MainServiceImpl implements MainService {
 
         Map<String, Integer> map = new TreeMap<>();
 
-        strings.parallelStream().forEach(s -> {
+        strings.forEach(s -> {
             map.putIfAbsent(s, 0);
-            map.put(s, map.get(s)+1);
+            map.put(s, map.get(s) + 1);
         });
 
         return map;
     }
 
-    @SneakyThrows
+
     private List<String> parseFile(MultipartFile file) {
-        validateFile(file);
 
-        InputStream inputStream = file.getInputStream();
-        BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream));
 
-        List<String> words = new ArrayList<>();
-        String line;
-        while ((line = reader.readLine()) != null) {
-            words.add(line);
+        InputStream inputStream = null;
+        try {
+            inputStream = file.getInputStream();
+            BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream));
+
+            List<String> words = new ArrayList<>();
+            String line;
+            while ((line = reader.readLine()) != null) {
+                line = line.toLowerCase();
+                words.addAll(Arrays.asList(line.split("[,;:.~!-?\\s]+")));
+            }
+
+            return words;
+        } catch (IOException e) {
+            e.printStackTrace();
         }
-
-        return words;
-    }
-
-    private void validateFile(MultipartFile file) {
-        if(!file.getContentType().equals("text/plain")) {
-            throw new RuntimeException("Wrong content type");
-        }
+        return null;
     }
 }
